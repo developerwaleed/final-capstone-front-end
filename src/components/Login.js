68b6eedf317cfe54147db38e32b15a579550c356
Charles <okoyecharles509@gmail.com>
2022-11-24 12:46:27 +0100

    Create Login and SignUp Components

---
+++ src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => (
+  <form action="" className="login-form auth-form">
+   
+  </form>
+);
+
+export default Login;
---
+++ src/components/SignUp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignUp = () => (
+  <form action="" className="signup-form auth-form">
+  
+  </form>
+);
+
+export default SignUp;
