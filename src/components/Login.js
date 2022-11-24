029621d88d70b17f9a5219b8e9406a1d7db8d255
Charles <okoyecharles509@gmail.com>
2022-11-24 12:46:59 +0100

    Fill in Login and SignUp Components

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 const Login = () => (
   <form action="" className="login-form auth-form">
-   
+    <div>
+      <label htmlFor="name">
+        Your Name
+        <input id="name" type="text" />
+      </label>
+    </div>
+    <div>
+      <label htmlFor="password">
+        Password
+        <input
+          id="password"
+          type="password"
+        />
+      </label>
+    </div>
   </form>
 );
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,7 +2,30 @@ import React from 'react';
 
 const SignUp = () => (
   <form action="" className="signup-form auth-form">
-  
+    <div>
+      <label htmlFor="name">
+        Your Name
+        <input id="name" type="text" placeholder="First and last name" />
+      </label>
+    </div>
+    <div>
+      <label htmlFor="email">
+        E-mail
+        <input id="email" type="email" />
+      </label>
+    </div>
+    <div>
+      <label htmlFor="password">
+        Password
+        <input id="password" type="password" placeholder="At least 6 characters" />
+      </label>
+    </div>
+    <div>
+      <label htmlFor="password_confirmation">
+        Confirm Password
+        <input id="password_confirmation" type="password" />
+      </label>
+    </div>
   </form>
 );
 
