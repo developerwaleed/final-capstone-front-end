c770f1a159f798ac94b163761d65bfee20d4448a
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-11-25 18:24:26 +0100

    Add routes for each component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import MakeReservation from './pages/makeReserve';
+import MyReservation from './pages/myReserve';
+import AddReservation from './pages/addReserve';
+import DeleteReservation from './pages/deleteReserve';
+import Navigation from './components/navigation';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/makereserve" element={<MakeReservation />} />
+          <Route path="/myreserve" element={<MyReservation />} />
+          <Route path="/addreserve" element={<AddReservation />} />
+          <Route path="/deletereserve" element={<DeleteReservation />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
