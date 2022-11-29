26d6390924bc6e79495c67b89becc857192569e1
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-11-29 10:20:59 +0100

    Conflict resolution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import './styles/App.css';
-import Home from './components/Home/Home';
-import DeleteFitnessActivity from './components/DeleteFitnessActivity';
-import ReserveFitnessActivity from './components/ReserveFitnessActivity';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/makereserve" element={<MakeReservation />} />
-          <Route path="/myreserve" element={<MyReservation />} />
-          <Route path="/addreserve" element={<AddReservation />} />
-          <Route path="/deletereserve" element={<DeleteReservation />} />
-        </Routes>
-      </Router>
-      <Home />
-      <DeleteFitnessActivity />
-      <ReserveFitnessActivity />
+      <h2>Check</h2>
     </div>
   );
 }
