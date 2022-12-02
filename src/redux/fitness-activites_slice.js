9f1ba7b6473c5ad73380b7cddea612bec32d3643
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-12-02 11:41:55 +0100

    Add delete action to reducer

--- src/redux/fitness-activites_slice.js
+++ src/redux/fitness-activites_slice.js
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
-import getFitnessActivites from './actions/fitness-activities';
+import DeleteFitnessActivity from '../components/DeleteFitnessActivity';
+import { getFitnessActivites } from './actions/fitness-activities';
 
 const initialState = {
   fitnessActivities: null,
@@ -10,10 +11,13 @@ const fitnessActivitiesSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getFitnessActivites.fulfilled, (state, action) => ({
-      ...state,
-      fitnessActivities: action.payload,
-    }));
+    builder
+      .addCase(getFitnessActivites.fulfilled, (state, action) => ({
+        ...state,
+        fitnessActivities: action.payload,
+      })
+        // eslint-disable-next-line max-len
+        .addCase(DeleteFitnessActivity.fulfilled, (state, action) => state.fitnessActivities.filter((item) => item.id !== action.payload)));
   },
 });
 
