8dc11bacc339de8eba740ac157ed4e5c6f8ea2fa
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-12-02 11:41:06 +0100

    Create method for delete action

--- src/redux/actions/fitness-activities.js
+++ src/redux/actions/fitness-activities.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import GET_FITNESS_ACTIVITIES from '../actionTypes';
+import { GET_FITNESS_ACTIVITIES, DELETE_FITNESS_ACTIVITY } from '../actionTypes';
 
 const getFitnessActivites = createAsyncThunk(
   GET_FITNESS_ACTIVITIES,
@@ -13,4 +13,9 @@ const getFitnessActivites = createAsyncThunk(
   },
 );
 
-export default getFitnessActivites;
+const deleteFitnessActivity = createAsyncThunk(DELETE_FITNESS_ACTIVITY, async (id) => {
+  await axios.delete(`${'http://localhost:3001/api/v1/fitness_activities'}/${id}`);
+  return id;
+});
+
+export { getFitnessActivites, deleteFitnessActivity };
