7008dea45a14a01f69abbd8e9c3fe36f3f66161b
Waleed Amjad <waleedamjad99@hotmail.com>
2022-12-09 13:12:53 +0500

    Update api-route.jsx

--- src/config/api-route.jsx
+++ src/config/api-route.jsx
@@ -1,3 +1,3 @@
-const API_ROUTE = process.env.BACKEND_URL || 'http://[::1]:3001';
+const API_ROUTE = process.env.BACKEND_URL || 'https://final-capstone-back-end-production.up.railway.app';
 
 export default API_ROUTE;
